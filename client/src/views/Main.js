de28d1edaa9b4bd45b27a498bad71b129c5c2143
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-09 13:51:27 -0700

    insert PersonForm into DOM

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import PersonForm from '../components/PersonForm';
+
 const Main = () => {
-  const [message, setMessage] = useState("Loading...")
-  useEffect(()=>{
-    axios.get("http://localhost:8000/api")
-    .then(res=>setMessage(res.data.message))
-  }, []);
+  
   return (
     <div>
-      <h2>Message from the backend: {message}</h2>
+      <PersonForm/ >
     </div>
   );
 }
