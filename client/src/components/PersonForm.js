6c1ae1fdcdc4c30eb951afe40ee877a2e234a9b8
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-09 12:52:46 -0700

    added submit handler preventing dreload

--- client/src/components/PersonForm.js
+++ client/src/components/PersonForm.js
@@ -4,3 +4,10 @@ import axios from 'axios';
 //keep track of what is being typed via useState hook
 const [firstName, setFirstName] = useState("");
 const [lastName, setLastName] = useState("");
+
+//handler when the form is submitted
+const onSubmitHandler = e => {
+  //prevent default behavior of the submit
+  e.preventDefault();
+  
+}
