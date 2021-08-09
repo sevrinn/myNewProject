6496898a8e19942d7753b01f769f346095125d4f
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-09 12:50:39 -0700

    keep track of what is being typed with useState hook

--- client/src/components/PersonForm.js
+++ client/src/components/PersonForm.js
@@ -2,3 +2,5 @@ import React, { useState} from 'react'
 import axios from 'axios';
 
 //keep track of what is being typed via useState hook
+const [firstName, setFirstName] = useState("");
+const [lastName, setLastName] = useState("");
