fba8c0ec3670162583e0946d5e21d56da8ee6c05
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-09 11:41:57 -0700

    added method to handle creation of new person

--- server/controllers/person.controller.js
+++ server/controllers/person.controller.js
@@ -5,3 +5,13 @@ module.exports.index = (request, response) => {
     message: "Hello World"
   });
 }
+
+module.exports.createPerson = (request, response) => {
+  const { firstName, lastName } = request.body;
+  Person.create({
+    firstName,
+    lastName 
+  })
+    .then(person => response.json(person))
+    .catch(err => response.json(err));
+}
