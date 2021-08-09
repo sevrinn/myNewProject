2f8c6c51ba513dbb74f567812a8415cc52b6d2fc
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-09 13:03:04 -0700

    made post request to create a new person

--- client/src/components/PersonForm.js
+++ client/src/components/PersonForm.js
@@ -9,5 +9,13 @@ const [lastName, setLastName] = useState("");
 const onSubmitHandler = e => {
   //prevent default behavior of the submit
   e.preventDefault();
+
+  //make a post request to create a new person
+  axios.post('http://localhost:8000/api/people', {
+    firstName, //shortcut syntax for firstName: firstName
+    lastName  //shortcut syntax for lastName: lastName
+  })
+    .then(res=>console.log(res))
+    .catch(err=>console.log(err))
   
 }
